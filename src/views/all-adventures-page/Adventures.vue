f00ef161c76c0fce811d72e0c5125bd01b212cc5
dtsintsarska <d.tsintsarska@gmail.com>
2021-02-27 15:20:29 +0200

    Add all adventures page with template for search

---
+++ src/views/all-adventures-page/Adventures.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+    <section class="background">
+      <Title title="Adventures" />
+      <h4>Live your life by a compass, not a clock!</h4>
+    </section>
+    <section class="search">
+      <Search />
+    </section>
+    <section class="adventures">
+      <Offers title="All Adventures" />
+    </section>
+  </main>
+</template>
+
+<script>
+import Offers from "../../components/Offers.vue";
+import Title from '../../components/Title.vue'
+import Search from "./Search.vue";
+
+export default {
+  components: {
+    Offers,
+    Title,
+    Search,
+  },
+};
+</script>
+
+<style>
+.background {
+  background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+    url("../../assets/images/aurora.jpg") no-repeat center;
+  background-size: cover;
+}
+
+.background h4 {
+  color: snow;
+  text-align: center;
+  font-size: 20px;
+  font-style: oblique;
+  padding-bottom: 2%;
+}
+</style>
