19d59d8c1e277bc7e778d9b90457e62dbc313c90
dtsintsarska <d.tsintsarska@gmail.com>
2021-03-06 18:04:13 +0200

    Add search by input field

---
+++ src/views/input-search-page/SearchByInput.vue
@@ -0,0 +1,60 @@
+<template>
+  <main>
+    <section class="background">
+      <Title title="Adventures" />
+      <h4>Live your life by a compass, not a clock!</h4>
+    </section>
+    <section class="search">
+      <Search />
+    </section>
+    <section class="adventures">
+        <Offers :title="`Results for '${searchInput}'`" :search="searchInput" />
+    </section>
+  </main>
+</template>
+
+<script>
+
+import Search from "../all-adventures-page/Search.vue";
+import Title from "../../components/Title.vue";
+import Offers from "../../components/Offers.vue";
+
+export default {
+    data() {
+        return {
+            hasResult: true,
+            searchInput: this.$route.params.regex
+
+        }
+    }, components: {
+        Search, 
+        Title, 
+        Offers
+    },
+
+  watch: {
+    $route(to, from) {
+
+      if (to !== from) {
+        this.searchInput = this.$route.params.regex;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.background {
+  background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+    url("../../assets/images/aurora.jpg") no-repeat center;
+  background-size: cover;
+}
+
+.background h4 {
+  color: snow;
+  text-align: center;
+  font-size: 20px;
+  font-style: oblique;
+  padding-bottom: 2%;
+}
+</style>
